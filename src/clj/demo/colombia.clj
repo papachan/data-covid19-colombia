952f0bbb5d80222da8b3059b6a89ac9f12ead401
Dan Loaiza <papachan@gmail.com>
2020-03-31 15:33:11 -0500

    update data for 31-03-2020

--- src/clj/demo/colombia.clj
+++ src/clj/demo/colombia.clj
@@ -1,6 +1,5 @@
 (ns demo.colombia
   (:require [clojure.set :as set]
-            [clojure.java.io :refer [reader]]
             [clojure.edn :as edn]
             [clojure.string :as str]
             [clojure.java.io :as io]
@@ -44,44 +43,51 @@
   ;; ;; count fallecidos en bogota
   (count (filter #(= "fallecido" %) statuses)) ;; => 3 => 5
 
-  (count rows) ;; => 491 => 539 => 608 => 702 => 798
+  (count rows) ;; => 491 => 539 => 608 => 702 => 798 => 906
 
   (count (filter #{"Bogotá"}
-           (map #(nth % 2) rows))) ;; => 225 => 264 => 297 => 353
+           (map #(nth % 2) rows))) ;; => 225 => 264 => 297 => 353 => 390
 
-  (count (filter #{"30/3/2020"}
+  (count (filter #{"31/3/2020"}
                  (map #(nth % 1) rows))) ;; =>
   ;; 27/3/2020 => 48
   ;; 28/3/2020 => 69
   ;; 29/3/2020 => 94
   ;; 30/3/2020 => 96
+  ;; 31/3/2020 => 108
 
   ;; statuses to lower case
-  (map #(clojure.string/lower-case (nth % 4)) (filter #(some #{"Bogotá"} %) rows))
+  ;; (map #(clojure.string/lower-case (nth % 4)) (filter #(some #{"Bogotá"} %) rows))
 
-  (map #(nth % 4)
-       (filter #(some #{"Bogotá"} %) rows))
+  ;; (map #(nth % 4)
+  ;;      (filter #(some #{"Bogotá"} %) rows))
 
   ;; numero de relacionados en bogota
-  (count (filter #(= "relacionado" %) types)) ;; => 71 ;; => 73 ;; => 89 ;; => 95
-  (count (filter #(= "importado" %) types)) ;; => 126 => 139 => 154 => 178
+  (count (filter #(= "relacionado" %) types)) ;; => 71 ;; => 73 ;; => 89 ;; => 95 => 115
+  (count (filter #(= "importado" %) types)) ;; => 126 => 139 => 154 => 178 => 183
+
+  ;; numero de casos relacionados y en estudio en bogota
+  (count (filter (fn [s] (or (= "relacionado" s) (= "en estudio" s))) types)) ;; => 207
 
   ;; por edades
-  ;; => {"20 a 29" 62, "70 a 79" 18, "60 a 69" 41, "50 a 59" 59, "40 a 49" 65, "10 a 19" 6, "80 a 89" 7, "30 a 39" 90, "0 a 9" 2}
+  ;; => {"20 a 29" 67, "70 a 79" 18, "60 a 69" 44, "50 a 59" 64, "40 a 49" 72, "10 a 19" 10, "80 a 89" 7, "30 a 39" 104, "0 a 9" 4}
   (frequencies ages)
 
   ;; suma por regiones
   (frequencies (map #(nth % 2) only-infected))
-  (by-regions "Bogotá") ;; => 294 => 350
+  (by-regions "Bogotá") ;; => 294 => 350 => 371
 
-  ;; => {"recuperado" 3, "en casa" 305, "hospital uci" 17, "hospital" 20, "fallecido" 5}
+  ;; => {"recuperado" 19, "casa" 316, "hospital" 31, "hospital uci" 19, "fallecido" 5}
   (frequencies statuses)
   ;; by-regions
 
   ;; Fallecidos total
   (count (filter #{"Fallecido"}
-           (map #(nth % 4) rows))) ;; => 14
+                 (map #(nth % 4) rows))) ;; => 14 ;; => 16
+
+  ;; nuevos casos por fechas
+  (sort (frequencies (into [] (map #(clojure.string/lower-case (nth % 1)) rows))))
 
   ;; fallecidos por regiones:
-  ;; => {"Santa Marta" 1, "Cali" 4, "Cartagena" 3, "Bogotá" 5, "Neiva" 1}
+  ;; => => {"Santa Marta" 1, "Cali" 4, "Cartagena" 3, "Bogotá" 5, "Pereira" 1, "Soledad" 1, "Neiva" 1}
   (frequencies (into [] (map #(nth % 2) (filter #(some #{"Fallecido"} %) rows)))))
