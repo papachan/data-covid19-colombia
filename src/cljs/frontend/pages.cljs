48f6a639401b7b2e98d01b21739f8514e0925247
Dan Loaiza <papachan@gmail.com>
2020-04-04 08:22:13 -0500

    update view with more stats

--- src/cljs/frontend/pages.cljs
+++ src/cljs/frontend/pages.cljs
@@ -6,6 +6,8 @@
             [frontend.date :as d])
   (:import goog.i18n.DateTimeFormat))
 
+(defonce state (atom [0 0]))
+
 (defn sum
   [coll]
   (loop [in coll,
@@ -29,7 +31,7 @@
 (defn show-chart
   [data]
   (let [options {:fullWidth true
-                 :height "380px"
+                 :height "420px"
                  :lineSmooth false}]
     (chartist/Line. ".ct-chart" (clj->js data) (clj->js options))))
 
@@ -49,7 +51,9 @@
             series1 (into [] (map #(nth % 1) res))
             series2 (into [] (map (fn [[k v]] v) (sum (map (fn [[k v]] [k v]) (sort (map (fn [[k v]] [(d/parse-date k) v]) result))))))
 
-            labels-fechas (into [] (map #(first (clojure.string/split (first %) #"/")) contamined))]
+            labels-fechas (into [] (map #(first (clojure.string/split (first %) #"/")) contamined))
+            _ (reset! state [(count (filter #(some #{"Fallecido"} %) data))
+                             (count (remove (fn [s] (= "recuperado" (clojure.string/lower-case (nth s 4)))) data))])]
         (reagent/create-class
          {:component-did-mount #(show-chart {:labels labels-fechas
                                              :series [series1 series2]})
@@ -58,13 +62,45 @@
                                  [:div {:id "chart4"
                                         :class "ct-chart"}])})))))
 
-(defn home
+(defn block-stats
+  [{:keys [title value]} data]
+  [:div
+   {:id "block-stats-1"}
+   [:div
+    {:className "title"}
+    title]
+   [:div
+    {:className "stats"}
+    value]])
+
+(defn footer
   []
   [:div
-   [:header
-    [:h1
-     "Colombia Covid19 Report"]
+   {:className "footer"}
+   "© papachan - "
+   [:a {:href "https://twitter.com/papachan"} "Twitter"]
+   " "
+   [:a {:href "https://github.com/papachan"} "Github"]])
+
+(defn home
+  []
+  (let [
+
+        [a b] @state]
     [:div
-     {:className "author"}
-     "Made by @papachan"]]
-   [chart-component]])
+     [:header
+      [:h1
+       "Colombia Covid19 Report"]
+      [:div
+       {:className "author"}
+       "Live report from colombian data using clojurescript"]]
+     [:div
+      {:className "container"}
+      [:div
+       {:className "graph"}
+       [chart-component]]
+      [:div
+       {:id "stats"}
+       [block-stats {:title "Number of deaths" :value a}]
+       [block-stats {:title "Number of contamined" :value b}]]]
+     [footer]]))
