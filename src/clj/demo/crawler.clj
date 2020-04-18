0aa298709d62fd11f00bc985a9c7e997e09e2911
Dan Loaiza <papachan@gmail.com>
2020-04-18 08:59:45 -0500

    adding normalize date method to crawler

--- src/clj/demo/crawler.clj
+++ src/clj/demo/crawler.clj
@@ -38,18 +38,6 @@
         (fetch-file str-date (clojure.string/join ["resources/" name]))
         (recur (dec i))))))
 
-(let [content (slurp (io/resource "datos.json"))
-      json-data (json/parse-string content)
-      start-date (->> (json-data "data")
-                      first
-                      rest
-                      vec
-                      (map second)
-                      first
-                      (f/parse (f/formatter "dd/MM/yyyyy")))
-      days-diff (t/in-days (t/interval start-date (t/now)))]
-  (start-crawler (- days-diff 1)))
-
 (defn process-data
   [file]
   (let [data (slurp file)]
@@ -78,54 +66,83 @@
         (recur (dec i) res))
       out)))
 
-(let [json-data (->> "datos.json"
-                      io/resource
-                      slurp
-                      json/parse-string)
-      fmt (f/formatter "dd/MM/yyyyy")
-      start-date (->> (json-data "data")
-                      first
-                      rest
-                      vec
-                      (map second)
-                      first
-                      (f/parse fmt))
-      days-diff (t/in-days (t/interval start-date (t/now)))
-      header ["ID de caso"
-                "Fecha de diagnóstico"
-                "Ciudad de ubicación"
-                "Departamento o Distrito"
-                "Atención**"
-                "Edad"
-                "Sexo"
-                "Tipo*"
-                "País de procedencia"]
-      content (parse-json-files (- days-diff 1) header)
-      sheet-names ["PositivasNegativas"
-                   "Titulo"
-                   "Casos1"
-                   "IndicadoresGenerales"
-                   "Mapa"
-                   "Copia de Mapa"
-                   "Mundo"
-                   "Procesadas"
-                   "PCR"
-                   "Etario"
-                   "TotalEtario"
-                   "importadosvsrelacionados"
-                   "Procedencia"
-                   "Estado"
-                   "Estado2"
-                   "Hoja 12"
-                   "Historico_Muestras"
-                   "fys"
-                   "Laboratorios operando en Colomb"]
-      data-json (when content
-                  {:data [content]
-                   :sheetNames ["Casos1"]
-                   :allSheetNames sheet-names
-                   :refreshed (coerce/to-long (t/now))})]
-  (spit "resources/datos.json" (json/encode data-json)))
+;; look for bad dates formats and fix it under our json
+(defn standarize-dates
+  [max-num]
+  (loop [i max-num
+         out 0]
+    (if (>= i 0)
+      (let [fmt (f/formatter "dd/MM/yyyy")
+            name (clojure.string/join ["temp" i ".json"])
+            body (slurp (io/resource name))
+            json-obj (json/parse-string body)
+            dat ((first json-obj) "fecha_de_diagn_stico")
+            vdat (clojure.string/split dat #"/")
+            new-date (if (and (count (nth vdat 2))
+                              (= (nth vdat 2) "20"))
+                       (f/unparse fmt (f/parse (f/formatter "dd/MM/yy") dat))
+                       (f/unparse fmt (f/parse (f/formatter "dd/MM/yyyy") dat)))
+            new-data (clojure.walk/prewalk-replace {["fecha_de_diagn_stico" dat] ["fecha_de_diagn_stico" new-date]} json-obj)
+            _ (if-not (nil? new-data)
+                (spit (str "resources/" name) (json/encode new-data)))]
+        (recur (dec i) (+ out (count json-obj))))
+      out)))
+
+(def fmt (f/formatter "dd/MM/yyyyy"))
+(def json-data (->> "datos.json"
+                    io/resource
+                    slurp
+                    json/parse-string))
+(def start-date (->> (json-data "data")
+                     first
+                     rest
+                     vec
+                     (map second)
+                     first
+                     (f/parse fmt)))
+
+(def days-diff (- (t/in-days (t/interval start-date (t/now))) 1))
+
+(def header ["ID de caso"
+             "Fecha de diagnóstico"
+             "Ciudad de ubicación"
+             "Departamento o Distrito"
+             "Atención**"
+             "Edad"
+             "Sexo"
+             "Tipo*"
+             "País de procedencia"])
+
+;; create new datos.json file
+(do
+  (start-crawler days-diff)
+  (standarize-dates days-diff)
+  (let [content (parse-json-files days-diff header)
+        sheet-names ["PositivasNegativas"
+                       "Titulo"
+                       "Casos1"
+                       "IndicadoresGenerales"
+                       "Mapa"
+                       "Copia de Mapa"
+                       "Mundo"
+                       "Procesadas"
+                       "PCR"
+                       "Etario"
+                       "TotalEtario"
+                       "importadosvsrelacionados"
+                       "Procedencia"
+                       "Estado"
+                       "Estado2"
+                       "Hoja 12"
+                       "Historico_Muestras"
+                       "fys"
+                       "Laboratorios operando en Colomb"]
+          data-json (when content
+                      {:data [content]
+                       :sheetNames ["Casos1"]
+                       :allSheetNames sheet-names
+                       :refreshed (coerce/to-long (t/now))})]
+      (spit "resources/datos.json" (json/encode data-json))))
 
 ;; export to csv
 (let [json-data (->> "datos.json"
