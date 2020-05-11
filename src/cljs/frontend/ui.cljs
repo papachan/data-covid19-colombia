26537bb78d1f7a16e20ec4175887424e9604adc9
Dan Loaiza <papachan@gmail.com>
2020-05-11 07:58:44 -0500

    add deads grouped by ages

--- src/cljs/frontend/ui.cljs
+++ src/cljs/frontend/ui.cljs
@@ -3,7 +3,8 @@
             [reagent.core :as reagent :refer [atom]]
             ["chartist" :as chartist]
             ["react-chartist" :default react-graph]
-            [frontend.data :as data :refer [process-data]]))
+            [frontend.data :as data :refer [process-data]]
+            [goog.string :refer [format]]))
 
 
 (defn row
@@ -67,18 +68,43 @@
                 :series [series]}
          :type "Bar"}]])))
 
+
+(def map-fields-name
+  (let [fields [:1_zeros_to_nine
+                :2_tens
+                :3_twenties
+                :4_thirties
+                :5_fourties
+                :6_fifties
+                :7_sixties
+                :8_seventies
+                :9_eighties
+                :10_nineties]
+        texts (vec (map #(format "%s a %s" % (+ % 9)) (range 0 100 10)))]
+    (apply hash-map (interleave fields texts))))
+
 (defn chart-bars-component2
   [{:keys [data title]}]
   (when-not (empty? data)
-    (let [series (->> data
-                      (filter #(some #{"Fallecido" "fallecido"} %))
-                      (map #(js/parseInt (nth % 6)))
-                      (group-by (fn [v] (< v 40)))
-                      vals
-                      (map count)
-                      (into []))
-          labels ["deads over 40 years old"
-                  "deads less than 40 years old"]
+    (let [segment-by-age (->> data
+                              (filter #(some #{"Fallecido" "fallecido"} %))
+                              (map #(nth % 6))
+                              (map js/parseInt)
+                              (group-by #(cond (<= 0 % 9)   :1_zeros_to_nine
+                                               (<= 10 % 19) :2_tens
+                                               (<= 20 % 29) :3_twenties
+                                               (<= 30 % 39) :4_thirties
+                                               (<= 40 % 49) :5_fourties
+                                               (<= 50 % 59) :6_fifties
+                                               (<= 60 % 69) :7_sixties
+                                               (<= 70 % 79) :8_seventies
+                                               (<= 80 % 89) :9_eighties
+                                               (>= % 90)    :10_nineties))
+                              (map (fn [[k vs]]
+                                     {(map-fields-name k) (count vs)}))
+                              (into (sorted-map)))
+          series (vals segment-by-age)
+          labels (keys segment-by-age)
           options {:height "220px"}]
       [:div
        {:id "chart6"}
