febce0fd7fa8955e59de0c06a1b1579173edade0
Dan Loaiza <dan@resonancias.org>
2020-05-21 16:38:07 -0500

    change at cider config

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,3 @@
 ((nil . ((cider-default-cljs-repl . shadow)
-         (cider-shadow-cljs-default-options . "app"))))
+         (cider-shadow-default-options . ":app")
+         (cider-custom-cljs-repl-init-form . "(do (require '[shadow.cljs.devtools.api :as shadow]) (shadow/watch :app) (shadow/nrepl-select :app))"))))
