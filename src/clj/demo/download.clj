fe7f5408ab4ed261eefdb7e3833f3d0d5b62d3c7
Dan Loaiza <dan@resonancias.org>
2020-06-13 18:31:03 -0500

    remove empty rows from covid tests report

--- src/clj/demo/download.clj
+++ src/clj/demo/download.clj
@@ -31,6 +31,20 @@
     (with-open [wrtr (io/writer output-file)]
       (.write wrtr (clojure.data.json/write-str vals)))))
 
+(defn filter-empty-rows
+  [dat]
+  (filter #(not (empty? (first %))) dat))
+
+(defn convert-to-json
+  [name fname]
+  (let [[header & rows] (csv/read-csv (slurp (io/resource name)))
+        vals (->> rows
+                  filter-empty-rows
+                  (mapv #(hash-map :date (first %) :accumulate (Integer/parseInt (clojure.string/replace (second %) #"," "")))))
+        output-file (clojure.string/join ["docs/" fname])]
+    (with-open [wrtr (io/writer output-file)]
+      (.write wrtr (clojure.data.json/write-str vals)))))
+
 (comment
   (do
    (download-csv "report.csv")
